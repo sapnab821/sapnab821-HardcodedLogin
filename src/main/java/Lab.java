324a2f191be250f7c29bea401d6176a32e5ecbea
sapnab821 <sapnabolikal@gmail.com>
2023-02-21 13:40:55 +0000

    Hardcoded Login

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -15,6 +15,14 @@ public class Lab {
      * @return true if there is a successful login, and false otherwise.
      */
     public boolean login(String username, String password){
-        return false;
+        if (username=="admin"){
+            if(password=="qwerty"){
+                return true;
+            } else {return false;}
+        } else if(username=="user"){
+            if(password=="password"){
+                return true;
+            }else{ return false;}
+        } else{ return false; }
     }
 }
